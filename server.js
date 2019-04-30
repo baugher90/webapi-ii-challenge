0f6c97af63b299aa0f9c5184c1d2c80de7d5c8c5
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-04-30 16:10:53 -0400

    server.js file setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const server = express();
+const postsRouter = require('./posts/postRouter.js')
+
+server.use(express.json());
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Ethans Post API</h>
+    <p>Welcome to the Ethans Post API</p>
+  `);
+});
+
+module.exports = server;
