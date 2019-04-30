2150963d9475c6553517f7f9bcf5642f8aadd805
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-04-30 17:14:54 -0400

    fixed all routers to meet mvps with proper error messages

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -3,71 +3,83 @@ const Posts = require("../data/db");
 const router = express.Router();
 //========================================Post API's
 router.post("/", async (req, res) => {
-    try {
-        const post = await Posts.insert(req.body);
-        res.status(201).json(post);
-    } catch(err) {
+  const post = req.body;
+  if (post.title && post.contents) {
+    Posts.insert(post)
+      .then(post => {
+        res.status(200).json(post);
+      })
+      .catch(err => {
         res.status(500).json({
-            message:"Error adding post"
-        })
-    }
+          message: "Error retrieving the posts"
+        });
+      });
+  } else {
+    res.status(400).json({
+      errorMessage: "There was an error while saving the post to the database."
+    });
+  }
 });
 //========================================Get API's
 router.get("/", async (req, res) => {
-    try {
-        const post = await Posts.find(req.body);
-        res.status(200).json(post);
-    } catch (err) {
-        res.status(500).json({
-            message: "Error retrieving the posts"
-          });
-    }
+  try {
+    const post = await Posts.find(req.body);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be retrieved."
+    });
+  }
 });
 //-----------------------------------
 router.get("/:id", async (req, res) => {
-    try {
-      const post = await Posts.findById(req.params.id);
-  
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: "Post not found" });
-      }
-    } catch (err) {
-      res.status(500).json({
-        message: "Error retrieving the post"
-      });
+  try {
+    const post = await Posts.findById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
     }
-  });
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be retrieved."
+    });
+  }
+});
 //========================================Update API's
 router.put("/:id", async (req, res) => {
-    try {
-      const post = await Posts.update(req.params.id, req.body);
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: "The post could not be found" });
-      }
-    } catch (err) {
-      res.status(500).json({
-        message: "Error updating the post"
-      });
+  try {
+    const post = await Posts.update(req.params.id, req.body);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "The post could not be found" });
     }
-  });
+  } catch (err) {
+    res.status(500).json({
+      message: "The post information could not be modified."
+    });
+  }
+});
 //========================================Delete API's
 router.delete("/:id", async (req, res) => {
-    try {
-      const post = await Posts.remove(req.params.id);
-      if (post > 0) {
-        res.status(200).json({ message: "The post has been nuked" });
-      } else {
-        res.status(404).json({ message: "The post could not be found" });
-      }
-    } catch (err) {
-      res.status(500).json({
-        message: "Error removing the post"
-      });
+  try {
+    const post = await Posts.remove(req.params.id);
+    if (post > 0) {
+      res.status(200).json({ message: "The post has been nuked" });
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
     }
-  });
+  } catch (err) {
+    res.status(500).json({
+      message: "The post could not be removed"
+    });
+  }
+});
 
 module.exports = router;
